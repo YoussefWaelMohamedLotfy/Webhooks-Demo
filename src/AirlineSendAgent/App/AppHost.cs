ef2f3be8bfd53c7ded654ee9517aaade5ae67032
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-06-17 01:39:49 +0200

    Added DI Support for AirlineSendAgent Console App

---
+++ src/AirlineSendAgent/App/AppHost.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace AirlineSendAgent.App;
+
+internal class AppHost : IAppHost
+{
+    public void Run()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+}
