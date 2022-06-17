84c9fd26826cb52ed9b34a304bdf2bfde3c53c51
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-06-17 02:02:30 +0200

    Added Queue Event Listener + Configured EFCore & ConfigurationBuilder

--- src/AirlineSendAgent/App/AppHost.cs
+++ src/AirlineSendAgent/App/AppHost.cs
@@ -7,9 +7,37 @@ namespace AirlineSendAgent.App;
 
 internal class AppHost : IAppHost
 {
+
+
     public void Run()
     {
-        Console.WriteLine("Hello World!");
+        var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+        var queueName = channel.QueueDeclare().QueueName;
+
+        channel.QueueBind(queue: queueName,
+            exchange: "trigger",
+            routingKey: "");
+
+        var consumer = new EventingBasicConsumer(channel);
+        Console.WriteLine("Listening on the message bus...");
+
+        consumer.Received += async (ModuleHandle, ea) =>
+        {
+            Console.WriteLine("Event is triggered!");
+
+
+
+        };
+
+        channel.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+        Console.ReadLine();
     }
 
 }
