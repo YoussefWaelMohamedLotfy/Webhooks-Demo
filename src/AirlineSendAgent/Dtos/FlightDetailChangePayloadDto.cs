c5e75883872de493de3f75b3aada44423ff345e4
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-06-17 02:09:43 +0200

    Added Dtos for app

---
+++ src/AirlineSendAgent/Dtos/FlightDetailChangePayloadDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelAgentWeb.Dtos;
+
+public class FlightDetailChangePayloadDto
+{
+    public string WebhookURI { get; set; } = default!;
+
+    public string Publisher { get; set; } = default!;
+
+    public Guid Secret { get; set; }
+
+    public string FlightCode { get; set; } = default!;
+
+    public decimal OldPrice { get; set; }
+
+    public decimal NewPrice { get; set; }
+
+    public string WebhookType { get; set; } = default!;
+}
